4102731cd59f32cc78b317035925fc5b84cc9bf3
jwm <jwm3926@gmail.com>
2023-01-15 16:15:24 -0500

    finished models and connection.js

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model, Types} = require('mongooose')
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trimmed: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+        },
+        thoughts: [
+                {
+                type: Schema.types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.types.ObjectId,
+                ref: 'User',
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+)
+
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length
+});
+
+const User = model('User', userSchema)
+
+module.exports = User
